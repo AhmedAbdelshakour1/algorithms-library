25eda027282f87a0a63b1803fd79e820c259102d
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-07-05 14:19:43 +0200

    Create Linear Modular Equation.cpp

---
+++ Linear Modular Equation.cpp
@@ -0,0 +1,40 @@
+ll extended_euclid(ll a, ll b, ll &x, ll &y) {
+    if (a < 0) {
+        ll r = extended_euclid(-a, b, x, y);
+        x *= -1;
+        return r;
+    }
+    if (b < 0) {
+        ll r = extended_euclid(a, -b, x, y);
+        y *= -1;
+        return r;
+    }
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    ll g = extended_euclid(b, a % b, y, x);
+    y -= (a / b) * x;
+    return g;
+}
+
+ll linearDiophantine(ll a, ll b, ll c, ll &x, ll &y, bool &found) {
+    ll g = extended_euclid(a, b, x, y);
+    if (found = c % g == 0)
+        x *= c / g, y *= c / g;
+    return g;
+}
+
+// solves the equation ax = b (mod n)
+vector<ll> modularEquation(ll a, ll b, ll n) {
+    vector<ll> sols;
+    ll x, y, g;
+    g = extended_euclid(a, n, x, y);
+    if (b % g != 0)
+        return sols; // no solutions
+    x = ((x * b / g) % n + n) % n; // from LDE,+ve mod
+    for (int i = 0; i < g; ++i)    // Bézout's identity
+        sols.push_back((x + i * n / g) % n);
+    sort(sols.begin(), sols.end());
+    return sols;
+}
