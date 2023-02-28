a90899d74cf8384c506d62965a56a7334146afe8
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-02-28 21:40:21 +0200

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,3 +1,4 @@
+// Memoization
 string s, t;
 int memo[105][105];
 int LCS(int i, int j){
@@ -8,4 +9,18 @@ int LCS(int i, int j){
     if(s[i] == t[j])
         return ret = LCS(i + 1, j + 1) + 1;
     return ret = max(LCS(i, j + 1), LCS(i + 1, j));
+}
+
+// Tabulation
+int dp[1001][1001];
+int LCS(string s, string t) {
+    for (int i = 1; i <= s.size(); ++i) {
+        for (int j = 1; j <= t.size(); ++j) {
+            if(s[i - 1] == t[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    int ans = dp[s.size()][t.size()];
+    memset(dp, 0, sizeof dp);
+    return ans;
 }
